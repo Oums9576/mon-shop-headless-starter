8c8cf7ae40b6815cc80c9d9573e38604c9c6a860
Oums <oumarniang@iMac-de-Oumar.lan>
2025-07-02 22:39:30 +0200

    Ajout de la page d'accueil

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'Arial' }}>
+      <h1>Bienvenue sur ma boutique Headless !</h1>
+      <p>Ce site est en cours de construction.</p>
+    </div>
+  );
+}
+
+
